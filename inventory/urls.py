9a4882bf921ba46c0e6ca0d424fc786564053551
InjiChoi <chldlswl1212@gmail.com>
2020-08-13 15:58:37 +0900

    requirements.txt 추가

--- inventory/urls.py
+++ inventory/urls.py
@@ -17,10 +17,13 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('item.urls')),
+
+    path('', lambda req:redirect('item:item_list')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
